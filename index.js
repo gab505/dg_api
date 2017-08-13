d200f21d48c6c91af6f60294a95b40011347d845
Donggyu Seo <donggyuseo@Donggyus-MacBook-Pro.local>
2017-08-13 16:24:10 -0700

    2nd

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 let port = process.env.PORT || 5000;
 
+app.use(express.static(_dirname + '/public'));
+
 app.get("/", (req, res) => {
     res.send("Please visit DG website");
 })
